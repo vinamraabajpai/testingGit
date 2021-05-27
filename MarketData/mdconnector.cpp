1221f55225af7852f67944ab952f87750f62d7d9
Vinamraa Bajpai <Vinamraa.bajpai@esteeadvisors.com>
2021-05-27 17:23:36 +0530

    dev 4

--- MarketData/mdconnector.cpp
+++ MarketData/mdconnector.cpp
@@ -1,11 +1,5 @@
 first change to develop branch
-<<<<<<< HEAD
-second change (hotfix) - not yet pushed to main.
-hotfix line 1
-dev after hf 1
-=======
 second change - not yet pushed to main. dev pending.
 new dev line 2
 line 3 in dev.
 line 4 in dev.
->>>>>>> develop
